6dadca4f90d8ef53cadaa9c8ed49b9add91419d2
Алексей Ларюшкин <lea_alex@MacBook-Pro-Aleksej.local>
2020-11-25 14:23:59 +0500

    create

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert('ок')
+function create(tag, object, text){
+    const props = object || {}
+    const element = document.createElement(tag)
+    if (props.id) element.id = id
+    if (props.className) element.className = className
+    if (text) element.innerText = text
+    return element
+}
+
+let div = create('div', null, 'ghbdtn')
+
+
+document.body.append(div)
