7cd2bc5711cc0069b57951990895c5558b31c635
Алексей Ларюшкин <lea_alex@MacBook-Pro-Aleksej.local>
2020-11-25 14:48:37 +0500

    children

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-function create(tag, object, text){
+function create(tag, object, children){
     const props = object || {}
     const element = document.createElement(tag)
     if (props.id) element.id = id
     if (props.className) element.className = className
-    if (text) element.innerText = text
-    if (style) {
-        
+    if (props.style) {
+        Object.keys(props.style).forEach(key => {
+            element.style[key] = props.style[key]
+        })
     }
+    if (props.data) {
+        Object.keys(props.data).forEach(key => {
+            element.dataset[key] = props.data[key]
+        })
+    }
+    if (children) element.innerText = text
     return element
 }
 
