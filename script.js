035c575e8096f11723f91f9b95848fb1cb875dc6
Алексей Ларюшкин <lea_alex@MacBook-Pro-Aleksej.local>
2020-11-25 14:31:06 +0500

    style

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ function create(tag, object, text){
     if (props.id) element.id = id
     if (props.className) element.className = className
     if (text) element.innerText = text
+    if (style) {
+        
+    }
     return element
 }
 
-let div = create('div', null, 'ghbdtn')
+let div = create('div', {style: {color: 'red', fontSize: '20px'}}, 'ghbdtn')
 
 
 document.body.append(div)
