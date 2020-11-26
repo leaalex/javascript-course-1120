7564f844b84dc0d84ae02098779639344136d36e
Алексей Ларюшкин <lea_alex@MacBook-Pro-Aleksej.local>
2020-11-26 11:44:31 +0500

    task

--- script.js
+++ script.js
@@ -1,96 +1,90 @@
 window.onload = function(){
-function create(tag, object, ...children){
-    const {id, className, style,  data, attrs, events, ...props} = object
-    const element = document.createElement(tag)
-    if (id) element.id = id
-    if (className) element.className = className
-    if (style) {
-        Object.keys(style).forEach(key => {
-            element.style[key] = style[key]
-        })
+function Creater(root){
+    this.root = root
+    this.elements = {}
+    Creater.prototype.add = function(tag, object, ...children){
+        const {id, className, style,  data, attrs, events, ref,  ...props} = object
+        const element = document.createElement(tag)
+        if (id) element.id = id
+        if (className) element.className = className
+        if (style) {
+            Object.keys(style).forEach(key => {
+                element.style[key] = style[key]
+            })
+        }
+        if (data) {
+            Object.keys(data).forEach(key => {
+                element.dataset[key] = data[key]
+            })
+        }
+        if (props) {
+            Object.keys(props).forEach(key => {
+                element[key] = props[key]
+            })
+        }
+        if (attrs){
+            Object.keys(attrs).forEach(key => {
+                element.addEventListener(key , attrs[key])
+            })
+         }
+         if (events){
+            Object.keys(events).forEach(key => {
+                element.addEventListener(key , events[key])
+            })
+         }
+         if (ref) this.elements[ref] = element
+         
+        if (children) element.append(...children)
+        return element
     }
-    if (data) {
-        Object.keys(data).forEach(key => {
-            element.dataset[key] = data[key]
-        })
+    Creater.prototype.publish = function(...elements){
+        this.root.append(...elements)
     }
-    if (props) {
-        Object.keys(props).forEach(key => {
-            element[key] = props[key]
-        })
-    }
-    if (attrs){
-        Object.keys(attrs).forEach(key => {
-            element.addEventListener(key , attrs[key])
-        })
-     }
-     if (events){
-        Object.keys(events).forEach(key => {
-            element.addEventListener(key , events[key])
-        })
-     }
-     
-    if (children) element.append(...children)
-    return element
 }
 
 
 
-// function create(tag, object, text, ...children) {
-//     const props = object || {}
-//     const element = document.createElement(tag)
-//     if (props.id) element.id = id
-//     if (props.className) element.className = props.className
-//     if (props.style) {
-//     Object.keys(props.style).forEach(key => {
-//     element.style[key] = props.style[key]
-//     })
-//     }
-//     if (props.data) {
-//     Object.keys(props.data).forEach(key => {
-//     element.dataset[key] = props.data[key]
-//     })
-//     }
-//     if (props.href) element.href = props.href
-//     if (props.src) element.src = props.src
-//     if (text) element.innerText = text
-//     if (children) element.append(...children)
-//     return element
-//     }
-    
-    // let card =
-    // create("div", { className: "card", style: { width: "18rem" } }, 
-    // create("img", {
-    // src: "https://cdn.icon-icons.com/icons2/1812/PNG/512/4213460-account-avatar-head-person-profile-user_115386.png",
-    // className: "card-img-top"
-    // }),
-    // create("div", { className: "card-body" }, 
-    // create("h5", { className: "card-title" }, "Иван Иванович"),
-    // create("p", { className: "card-text" }, "Профессия: Программист"),
-    // create("p", { className: "card-text" }, "Стаж: 10 лет"),
-    // create("p", { className: "card-text" }, "Возраст: 30 лет"),
-    // create("a", { className: "btn btn-primary", href: "http://google.com" }, "Кнопка")
-    // )
-    // )
+window.creator = new Creater(document.getElementById('root'))
 
-
-
-    // document.body.append(card)
-
-
-
-    let input = create('input', {type: 'text'})
-
-    document.body.append(
-        create('div', {}, 
-            input,
-            create('button', { 
+creator.publish(
+    creator.add('div', {className: 'input-group mt-1 mb-3'}, 
+        creator.add('input', {
+            className: 'form-control', 
+            ref: 'input', 
+            type: 'text', 
+            placeholder: 'Введите описание задачи',
+            events: {
+                'keyup': function(event) {
+                    if (event.code === 'Enter'){
+                        creator.elements['container'].append(
+                            creator.add('p', {}, this.value)
+                            )
+                        this.value = ''
+                    }
+                    if (event.code === "Escape"){
+                        this.value = ''
+                    }
+                    let test  = () => console.log(this)
+                    test()
+                }
+            }
+        }),
+        creator.add('div', {className: 'input-group-append'},  
+            creator.add('button', { 
+                className:'btn btn-outline-secondary',
+                type: 'button',
                 events: {
-                    'click': ()=>{console.log(input.value)
+                    'click': ()=>{
+                        creator.elements['container'].append(
+                            creator.add('p', {}, creator.elements['input'].value)
+                            )
+                        creator.elements['input'].value = ''
                     }
                 }
-            }, 'Нажми на меня')
-            )
-        )
+            }, '+ Создать задачу')
+        ),
+    ),
+    creator.add('div', {ref: 'container'})
+    )
 
 }
